c5d9cb1d6b0318dca7da88172b5243d856cd0b14
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-01-18 14:01:27 +0100

    update task1.py

--- venv/task1.py
+++ venv/task1.py
@@ -1,4 +1,14 @@
 """Create a program that asks the user to enter their name and their age.
 Print out a message addressed to them that tells them the year that they will turn 100 years old."""
 
-print ("test")
+from datetime import date
+
+user_name = input("Please enter your name: ").capitalize()
+user_age = int(input("Please enter your age: "))
+# I assume that user will not enter 0 or negative value
+today = date.today()
+
+# current year minus age of a user plus 100 years
+year_user_age_100 = today.year - user_age + 100
+
+print(f"Hello {user_name}, if you are a lucky man, you will turn 100 years old at {year_user_age_100}.")
