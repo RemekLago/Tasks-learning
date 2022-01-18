16d42343fc1535ebc1851e1a1384a245ab7f12cb
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-01-18 14:28:58 +0100

    update task2.py

--- venv/task2.py
+++ venv/task2.py
@@ -1,10 +1,15 @@
 """Ask the user for a number.
 Depending on whether the number is even or odd, print out an appropriate message to the user.
-Hint: how does an even / odd number react differently when divided by 2?"""
+Hint: how does an even / odd number react differently when divided by 2?
+Extras:
+1. If the number is a multiple of 4, print out a different message."""
 
 user_number = int(input("Write your number: "))
 
-if user_number % 2 == 0:
+if user_number % 2 == 0 and user_number % 4 != 0:
     print(f"The number you put in ({user_number}) is even")
+elif user_number % 4 == 0:
+    print(f"The number you put in ({user_number}) is odd and is multiple of 4")
 else:
     print(f"The number you put in ({user_number}) is odd")
+
