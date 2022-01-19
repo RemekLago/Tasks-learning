d76f537b776379b20358de6614ede89a6a33bf17
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-01-19 09:41:46 +0100

    add task3.py

---
+++ venv/task3.py
@@ -0,0 +1,29 @@
+"""Take a list, say for example this one:
+  a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+and write a program that prints out all the elements of the list that are less than 5."""
+"""Extras:
+1. Instead of printing the elements one by one, make a new list that has all the elements less than 5 from this list in it and print out this new list.
+2. Write this in one line of Python.
+3. Ask the user for a number and return a list that contains only elements from the original list a that are smaller than that number given by the user.
+"""
+
+
+list = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+
+for i in list:
+    if i < 5:
+        print(i)
+
+# extras 1
+new_list = []
+for i in list:
+    if i < 5:
+        new_list.append(i)
+print(new_list)
+
+# extras 2 - list comprehensions
+print([i for i in list if i < 5])
+
+# extras 3
+number = int(input("please enter your number: "))
+print([i for i in list if i < number])
