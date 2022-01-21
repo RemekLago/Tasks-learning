799d1ad91549e57a45cfbdf6cbedbdf1c9cb408d
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-01-21 08:41:25 +0100

    add

---
+++ venv/task20.py
@@ -0,0 +1,10 @@
+"""Write a function that takes an ordered list of numbers (a list where the elements are in order
+from smallest to largest) and another number.
+The function decides whether or not the given number is inside the list and returns (then prints) an appropriate boolean.
+Extras:
+    Use binary search.
+"""
+import random
+
+list = [i for i in range(1, 13)]
+print(list)
