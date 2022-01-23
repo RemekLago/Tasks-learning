7ff4fd598269ac10019551a8d74572e15faa2869
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-01-23 13:13:25 +0100

    add

--- venv/task16.py
+++ venv/task16.py
@@ -6,15 +6,22 @@ Include your run-time code in a main method.
 Extra:
 1. Ask the user how strong they want their password to be. For weak passwords, pick a word or two from a list.
 """
+import string
 
 #length = input("How long your password should be: ")
 
-myDict = [1, 2, 3, 4, 5]
+myDict = ["1", "2", "3", "4", "5"]
 mySeparator = ""
 
 x = mySeparator.join(myDict)
 
 print(x)
+# wybierz z jakiego rodzaju danych losujemy
+# wybierz losowo cyfre 0-9
+# wybierz losow litere a-z
+# wybierz losowo znak specjalny
 
-
-
+print(string.digits[0:10])
+print(string.ascii_lowercase)
+print(string.ascii_uppercase)
+print(string.printable[-38:-1])
