e5c636582c2ac462be6d277cd66cc783fda7ce8d
Remigiusz <remigiusz.lagodzki@gmail.com>
2022-01-24 18:46:18 +0100

    add

--- venv/task16.py
+++ venv/task16.py
@@ -7,6 +7,7 @@ Extra:
 1. Ask the user how strong they want their password to be. For weak passwords, pick a word or two from a list.
 """
 import string
+import random
 
 #length = input("How long your password should be: ")
 
@@ -15,13 +16,62 @@ mySeparator = ""
 
 x = mySeparator.join(myDict)
 
-print(x)
-# wybierz z jakiego rodzaju danych losujemy
-# wybierz losowo cyfre 0-9
-# wybierz losow litere a-z
-# wybierz losowo znak specjalny
+#print(x)
 
-print(string.digits[0:10])
-print(string.ascii_lowercase)
-print(string.ascii_uppercase)
-print(string.printable[-38:-1])
+
+# string.digits[0:10])          random digit 0-9
+# string.ascii_lowercase)       random lower letter a-z
+# string.ascii_uppercase)       random upper letter A-Z
+# string.printable[-38:-1])     random special character !@#
+
+def create_password(length):
+    password = []
+    i = 0
+    while i <= length:
+        random_choice_of_base()
+        print(f"wylosowany: {random_choice_of_base()}")
+        if random_choice_of_base() == base1:
+            random_digit()
+            password.append(x)
+        elif random_choice_of_base() == base2:
+            random_lower_letter()
+            password.append(x)
+        elif random_choice_of_base() == base3:
+            random_upper_letter()
+            password.append(x)
+        elif random_choice_of_base() == base4:
+            random_special_character()
+            password.append(x)
+        i += 1
+    print(password)
+
+def random_choice_of_base():
+    base1 = random_digit()
+    base2 = random_lower_letter()
+    base3 = random_upper_letter()
+    base4 = random_special_character()
+    base = ["base1", "base2", "base3", "base4"]
+    choice = random.choice(base)
+    print(choice)
+
+def random_digit():
+    base = string.digits[0:10]
+    y = random.choice(base)
+    return(y)
+
+def random_lower_letter():
+    base = string.ascii_lowercase
+    x = random.choice(base)
+    return(x)
+
+def random_upper_letter():
+    base = string.ascii_uppercase
+    x = random.choice(base)
+    return(x)
+
+def random_special_character():
+    base = string.printable[-38:-1]
+    x = random.choice(base)
+    return(x)
+
+create_password(5)
